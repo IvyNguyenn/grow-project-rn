a8346950d9e216ef154c898ff472aa6846a1d00e
HoangVy <hoangvynguyen42@gmail.com>
2019-05-13 18:41:36 +0700

    Implement icon into tabBottomNavigator in ios

--- src/Screens/ProfileScreen/index.js
+++ src/Screens/ProfileScreen/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { colors } from "../../Assets/Theme";
 
 export class ProfileScreen extends Component {
     static navigationOptions = {
+        tabBarIcon: props => (
+            <Icon name="user" size={25} color={props.focused ? colors.mainBackgroundColor : "gray"} />
+        )
     };
 
     render() {
         return (
-            <View Style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <Text> Profile Screen </Text>
             </View>
         )
--- src/Screens/SettingScreen/index.js
+++ src/Screens/SettingScreen/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { colors } from "../../Assets/Theme";
 
 export class SettingScreen extends Component {
     static navigationOptions = {
+        tabBarIcon: props => (
+            <Icon name="gear" size={25} color={props.focused ? colors.mainBackgroundColor : "gray"} />
+        )
     };
 
     render() {
         return (
-            <View Style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <Text> Setting Screen </Text>
             </View>
         )
